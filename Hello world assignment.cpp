4bb470399342af705108fa5cfcf8d66ffdd9186b
Jimmoman <71090206+Jimmoman@users.noreply.github.com>
2021-09-08 08:14:48 -0700

    Add files via upload

---
+++ Hello world assignment.cpp
@@ -0,0 +1,67 @@
+// Author: Jared Morris
+// Purpose: Name's Morris, Jared Morris
+// Psuedocode: It is printing out my name
+// Maintenance Log: 1st version, "Hello world my name is Jared Morris" assignment
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <string.h>
+#include <iomanip>
+
+
+
+using namespace std;
+
+int main()
+{
+	printf("Hello World!\nMy Name is Jared Morris!");
+
+	
+}
+//j = rand() % 12 + 1;
+//std::cout << "the d12 rolls a " << j << "\n";
+
+//float randomFraction = (float)rand() / RAND_MAX;
+//float c = 5.0f * randomFraction;
+//std::cout << "The computer floatd a " << c << "\n";
+
+//char x = 'A' + rand() % 26;
+//std::cout << "The only letter in your tome is " << x << "\n";
+
+//int range = inputHigh - inputLow;
+//int randomNumber = rand() % range + inputLow;
+
+//#include <cstdlib>
+//#include <time.h>
+
+//int main()
+
+//time_t t;
+//srand((unsigned)time(&t));
+
+//std::setprecision(3)
+//std::string name;
+
+//using namespace std;
+
+//cin >> Num;
+
+//switch (Num)
+//{
+//case 'C':
+	//cout << "Excellent! Onto the next one..." << endl;
+	//Total += 1.0f;
+	//break;
+//case 'B':
+//case 'A':
+//case 'D':
+	//cout << "Excellent! Onto the next one..." << endl;
+	//Total += -0.25f;
+	//break;
+//default:
+	//cout << "A, B, C, or D please" << endl;
+	//return 1;
+
+	//printf("easily\nthe best\ncode");
+//}
